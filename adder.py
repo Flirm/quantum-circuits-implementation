4715c8b214a6c2f5d26d87cdedddd5ef567c49ad
Francisco de Lima Ramos <francisco.lima.ramos@gmail.com>
2025-04-08 20:09:25 -0300

    further improved circuit functions
    removed unnecessary inv_qc_carry
    remade qc_carry, now gets num_qubits as input and returns the circuit

--- adder.py
+++ adder.py
@@ -1,6 +1,6 @@
 #implementação do circuito de adição proposto por Vedral, Barenco e Ekert, utilizando 3n bits, onde n é número de bits de cada operando
 from qiskit import *
-
+from helpers import *
 
 #qc_carry function defines a 4 qubit circuit (carryIn, a, b, carryOut) sets the carry out value accondingly for the sum of a,b,carryIn
 def qc_carry() -> QuantumCircuit:
@@ -14,13 +14,6 @@ def qc_carry() -> QuantumCircuit:
     quantum_circuit.cx(1, 2)
     quantum_circuit.ccx(0, 2, 3)
 
-    return quantum_circuit
-
-#inv_qc_carry is the inverse/reversed version of qc_carry
-def inv_qc_carry(quantum_circuit: QuantumCircuit, qubits: list[int]) -> QuantumCircuit:
-    quantum_circuit.ccx(qubits[0], qubits[2], qubits[3]) 
-    quantum_circuit.cx(qubits[1], qubits[2]) 
-    quantum_circuit.ccx(qubits[1], qubits[2], qubits[3]) 
     return quantum_circuit
 
 #qc_sum function defines a 3 qubit quantum circuit (a, b, s) and sets the value of s according to the result of the sum from a and b
@@ -38,12 +31,21 @@ def qc_sum() -> QuantumCircuit:
 #in order from least significant to most significant
 #first n bits are from operand a, next n bits from b, extra bit for the last carry for b, the rest are n work bits c
 #|c>|0>|b>|a>
-def qc_adder(quantum_circuit: QuantumCircuit, n: int) -> QuantumCircuit:
-    for i in range(n):
-        quantum_circuit = qc_carry(quantum_circuit, [2*n+1+i, i, n+i, 2*n+2+i if i != n-1 else 2*n])
-    quantum_circuit.cx(n-1, 2*n-1)
-    quantum_circuit = qc_sum(quantum_circuit, [3*n, n-1, 2*n-1])
-    for i in range(n-1):
-        quantum_circuit = inv_qc_carry(quantum_circuit, [3*n-1-i, n-2-i, 2*n-2-i, 3*n-i])
-        quantum_circuit = qc_sum(quantum_circuit, [3*n-1-i, n-2-i, 2*n-2-i])
+def qc_adder(num_qubits: int) -> QuantumCircuit:
+    a = QuantumRegister(num_qubits, name="a")
+    b = QuantumRegister(num_qubits, name="b")
+    zero = QuantumRegister(1, name="0")
+    c = QuantumRegister(num_qubits, name="c")
+    quantum_circuit = QuantumCircuit(a, b, zero, c)
+
+    carry_circ = qc_carry()
+    sum_circ = qc_sum()
+
+    for i in range(num_qubits):
+        quantum_circuit.compose(carry_circ, qubits=[2*num_qubits+1+i, i, num_qubits+i, 2*num_qubits+2+i if i != num_qubits-1 else 2*num_qubits], inplace=True)
+    quantum_circuit.cx(num_qubits-1, 2*num_qubits-1)
+    quantum_circuit.compose(sum_circ,  qubits=[3*num_qubits, num_qubits-1, 2*num_qubits-1], inplace=True)
+    for i in range(num_qubits-1):
+        quantum_circuit.compose(carry_circ.inverse(), qubits=[3*num_qubits-1-i, num_qubits-2-i, 2*num_qubits-2-i, 3*num_qubits-i], inplace=True)
+        quantum_circuit.compose(sum_circ, qubits=[3*num_qubits-1-i, num_qubits-2-i, 2*num_qubits-2-i], inplace=True)
     return quantum_circuit
