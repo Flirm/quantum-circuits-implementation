00dde4bebca46f7ffe4162f246563c355e232cac
Francisco de Lima Ramos <francisco.lima.ramos@gmail.com>
2025-04-08 19:43:31 -0300

    created helper.py
    file holds generic funcions used in many of the circuits

---
+++ helpers.py
@@ -0,0 +1,18 @@
+import qiskit
+
+#returns a list with the indexes of all qubits in qregs
+def get_qubits(qc: qiskit.QuantumCircuit, qregs: list[qiskit.QuantumRegister]) -> list[int]:
+    bits = []
+    for qreg in qregs:
+        n = qc.find_bit(qreg[0]).index
+        bits.extend(list(range(n, n+qreg.size)))
+    return bits
+
+#applies a not in all bits of a given register acconding to control bit
+def cx_reg(num_qubits: int) -> qiskit.QuantumCircuit:
+    reg = qiskit.QuantumRegister(num_qubits, name="reg")
+    c = qiskit.QuantumRegister(1, name="c")
+    qc = qiskit.QuantumCircuit(reg,c)
+    for qubit in reg:
+        qc.cx(c[0], qubit)
+    return qc
