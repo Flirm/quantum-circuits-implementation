11436dfc031c3e502268bec5d5912363f327decd
Francisco de Lima Ramos <francisco.lima.ramos@gmail.com>
2025-04-13 13:37:40 -0300

    set up qubits

--- implementations/VBE/multiplier_VBE.py
+++ implementations/VBE/multiplier_VBE.py
@@ -5,5 +5,10 @@ from implementations.utils.helpers import *
 #given a fixed a and N, does the following effect
 #|c;x,0> ->  -{ |c;x,a*x mod N> if |c>==|1>
 #            \{ |c;x,x>         if |c>==|0> 
-def c_mult_mod_VBE(num_buqits: int) -> QuantumCircuit:
-    return
+def c_mult_mod_VBE(num_qubits: int, a: int, N: int) -> QuantumCircuit:
+    x = QuantumRegister(num_qubits, name="x")
+    c = QuantumRegister(1, name="c")
+    zero_x = QuantumRegister(num_qubits, name="0x")
+    zero_y = QuantumRegister(num_qubits, name="0y")
+    quantum_circuit = QuantumCircuit(c, x, zero_x, zero_y)
+    return quantum_circuit
