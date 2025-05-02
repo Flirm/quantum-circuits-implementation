2814a23888cd75d9722a58ee4a610f44759d2f13
Francisco de Lima Ramos <francisco.lima.ramos@gmail.com>
2025-05-02 16:27:30 -0300

    docs: mod adder documentation and fix others

--- implementations/VBE/adder_VBE.py
+++ implementations/VBE/adder_VBE.py
@@ -16,7 +16,7 @@ def qc_carry() -> QuantumCircuit:
     cOut: --⨁---⨁--
     
     Returns:
-        quantum_circuit(QuantumCircuit):
+        quantum_circuit(QuantumCircuit): the circuit implementing the operation.
     """
     cIn = QuantumRegister(1, name="cIn")
     a = QuantumRegister(1, name="a")
@@ -42,7 +42,7 @@ def qc_sum() -> QuantumCircuit:
     b: -⨁-⨁--
     
     Returns:
-        quantum_circuit(QuantumCircuit):
+        quantum_circuit(QuantumCircuit): the circuit implementing the operation.
     """
     c = QuantumRegister(1, name="c")
     a = QuantumRegister(1, name="a")
@@ -59,7 +59,7 @@ def qc_sum() -> QuantumCircuit:
 #first n bits are from operand a, next n bits from b, extra bit for the last carry for b, the rest are n work bits c
 #|c>|0>|b>|a>
 def adder_VBE(num_qubits: int) -> QuantumCircuit:
-    """Implements the circuit to sum two n-bit numbers.
+    """Implements the circuit to sum two :attr:`n-bit` numbers.
 
     It works by first calculating all carry bits, applyign n carry gates taking the i-th c[i], a[i], b[i], c[i+1] as entries.
 
@@ -100,13 +100,13 @@ def adder_VBE(num_qubits: int) -> QuantumCircuit:
 
     Complexity:
     -
-    The network depth as well as number of gates can be described in O(n).
+    The network depth as well as number of gates can be described in `O(n)`.
 
-    As for space, assuming n as the number of bits to encode the largest operand, we will have a total of:
+    As for space, assuming `n` as the number of bits to encode the largest operand, we will have a total of:
 
-    - 2n bits for operands a and b
-    - 1 bit for the final carry
-    - n bits for other carries
+    - `2n` bits for operands `a` and `b`
+    - `1` bit for the final carry
+    - `n` bits for other carries
 
     Args:
         num_qubits (int): number of bits from operands.
@@ -141,18 +141,87 @@ def adder_VBE(num_qubits: int) -> QuantumCircuit:
 #returns a circuit that calculates (a+b)%N, where N is given and computed classically, a and b both have n qubits
 #it is promissed that 0 <= a,b < N
 def mod_adder_VBE(num_qubits: int, N:int) -> QuantumCircuit:
-    """
+    """Implements the circuit to sum two :attr:`n-bit` numbers modulo :attr:`N`.
+
+    For the circuit to work properly it is given as promisse that `0 <= a,b < N`
+
+    .. note::
+        This version has some differences when comparred to `[1]`, this is because the original description has some errors
+        in it, this implementation corrects them.
+
+    We can divide the circuit's working in 4 parts:
+
+    - Use the plain adder to sum `a` and `b` and store the result in `b`.
+    - Use the reverse adder with the registers `(N, b)` as input:
+        - If `N > a + b`, it will cause underflow.
+        - Check the most significant bit for underflow and set the `controll-bit` accordingly.
+    - Now we check the `controll-bit` state:
+        - If negative, there was no underflow, this means we don't need to undo the operation, so we zero out the N register.
+        - If positive, there was underflow, this means `a + b` was smaller than `N` and we need to undo the operation.
+    - After checking the `controll-bit` and doing setting the N register we apply the plain adder with `(N, b)` again as input:
+        - Remember: if `c-bit` was negative, `N` will have `|0>`, else it will be `N`.
+    - Lastly, to reset the controll-bit to 0, we take the inverse adder with `(a, b)` as input:
+        - It will cause underflow if `a > b`, and this will only be case when `b` reg is `a+b-N`, meaning the c-bit is already at `0`.
+        - If there is no underflow, it means `b` content is `a+b`, this only happends when the `c-bit` is set to `1`.
+        - To reset the `c-bit`, we can use a negatively controlled `cnot` at `b`'s most significant bit targetign the `c-bit`.
+    - Then, we just apply the plain adder with `(a, b)` as input to undo the effects from the last inverse adder. 
+
+    The plain adder circuit has the following effect:
+
+    (a,b) -> (a, a+b)   if a+b < N
+
+    (a,b) -> (a, a+b-N) if a+b >= N
+
+    If applied in reverse:
+
+    (a,c) -> (a, c-a)   if c >= a
+
+    (a,c) -> (a, N+c-a) if c < a
+
+    Exemple mod-adder with 2-bit operands and N = 3:
+                 _____                                _____           _____
+    a0    ------|0    |------------------------------|0    |---------|0    |---
+                |     |                              |     |         |     |
+    a1    ------|1    |------------------------------|1    |---------|1    |---
+                |  -> |    _____            _____    | <-  |         |  -> |
+    b0    ------|2 A  |---|2    |----------|2    |---|2 A  |---------|2 A  |---
+                |  D  |   |     |          |     |   |  D  |         |  D  |
+    b1    ------|3 D  |---|3    |----------|3    |---|3 D  |---------|3 D  |---
+                |  E  |   | <-  |          |  -> |   |  E  |         |  E  |
+    cO    ------|4 R  |---|4 A  |--●-------|4 A  |---|4 R  |--X-●-X--|4 R  |---
+                |     |   |  D  |  |       |  D  |   |     |    |    |     |
+    anc0  ------|5    |---|5 D  |--|-------|5 D  |---|5    |----|----|5    |---
+                |     |   |  E  |  |       |  E  |   |     |    |    |     |
+    anc1  ------|6____|---|6 R  |--|-------|6 R  |---|6____|----|----|6____|---
+                          |     |  |   _   |     |   _          |
+    N0    -X--------------|0    |--|--| |--|0    |--| |---------|--------------
+                          |     |  |  | |  |     |  | |         |
+    N1    -X--------------|1____|--|--|_|--|1____|--|_|---------|--------------
+                                   |   |             |          |
+    0     -------------------------⨁-X-●-X---------X-●-X-------⨁--------------
+
+
+    Complexity:
+    -
+    The network depth as well as number of gates can be described in `O(n)`.
+    
+    For space, assuming `n` as the number of bits to encode `N`, we will have a total of:
+
+    - `3n` bits for `a`, `b` and `N`.
+    - `1` bit for the final carry.
+    - `1` bit to manage controlled operatons.
+    - `n` extra bits for the plain adder.
     
     Args:
         num_qubits (int): number of bits from operands.
         N (int): the modulo number, it's binary representation can have at max num_qubits.
 
     Returns:
-        quantum_circuit(QuantumCircuit):
+        quantum_circuit(QuantumCircuit): the circuit implementing the operation.
 
     Reference: 
     -
-        Quantum Networks for Elementary Arithmetic Operations.
+        [1] Quantum Networks for Elementary Arithmetic Operations.
         V. Vedral, A. Barenco, A. Ekert
     """
     #init work qubits and circuit
