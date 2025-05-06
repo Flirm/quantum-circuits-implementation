a423f2e8ee6fd8621ccfde39b746c09bc0ae298d
Francisco de Lima Ramos <francisco.lima.ramos@gmail.com>
2025-05-06 20:19:21 -0300

    added name to qcs

--- implementations/CDKM/adder_CDKM.py
+++ implementations/CDKM/adder_CDKM.py
@@ -2,6 +2,7 @@ from qiskit import *
 
 def qc_MAJ():
     quantum_circuit = QuantumCircuit(3)
+    quantum_circuit.name = "MAJ"
     quantum_circuit.cx(2, 1)
     quantum_circuit.cx(2, 0)
     quantum_circuit.ccx(0, 1, 2)
@@ -12,6 +13,7 @@ def qc_UMA(two_version=True):
     #if argument is true, implements the 2cnot version, else, implements the 3cnot version
     #check reference
     quantum_circuit = QuantumCircuit(3)
+    quantum_circuit.name = "UMA"
     if two_version:
         quantum_circuit.ccx(0, 1, 2)
         quantum_circuit.cx(2, 0)
