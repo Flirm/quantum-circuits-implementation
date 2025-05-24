7d07dc8f473fbf66f6f2d34006c3f264e66cd10e
Francisco de Lima Ramos <francisco.lima.ramos@gmail.com>
2025-05-24 17:04:38 -0300

    encode function

--- implementations/windowed_arithmetic/table_lookup.py
+++ implementations/windowed_arithmetic/table_lookup.py
@@ -2,3 +2,13 @@ from qiskit import *
 from math import log2, ceil
 from qiskit.circuit.library import XGate, UnitaryGate
 from qiskit.circuit import Gate
+
+def encode_table(l: list[int], size: int) -> list[str]:
+    """
+    l: list to be encoded
+    size: how many bits elements should have in it's encoding
+    """
+    encoded_l = []
+    for num in l:
+        encoded_l.append((bin(num)[2:]).rjust(size,"0"))
+    return encoded_l
