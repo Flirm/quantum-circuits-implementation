f898c1ae4ab2b4cea7945152dd1eb159e26d82e1
Francisco de Lima Ramos <francisco.lima.ramos@gmail.com>
2025-05-24 17:04:56 -0300

    control string generator

--- implementations/windowed_arithmetic/table_lookup.py
+++ implementations/windowed_arithmetic/table_lookup.py
@@ -12,3 +12,13 @@ def encode_table(l: list[int], size: int) -> list[str]:
     for num in l:
         encoded_l.append((bin(num)[2:]).rjust(size,"0"))
     return encoded_l
+
+def generate_control_strings(size: int) -> list[str]:
+    """
+    size: number of bits to encode list size
+    """
+    c_strings = []
+    string_size = ceil(log2(size))
+    for i in range(size):
+        c_strings.append((bin(i)[2:]).rjust(string_size,"0"))
+    return c_strings
