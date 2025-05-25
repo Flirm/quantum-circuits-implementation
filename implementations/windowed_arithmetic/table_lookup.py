7fd655b0b156e8ee915a5296d126882aabf19b43
Francisco de Lima Ramos <francisco.lima.ramos@gmail.com>
2025-05-25 15:23:32 -0300

    docs: compute_lookup_table

--- implementations/windowed_arithmetic/table_lookup.py
+++ implementations/windowed_arithmetic/table_lookup.py
@@ -44,7 +44,7 @@ def generate_control_strings(size: int) -> list[str]:
     return c_strings
 
 
-def xor_data_gates(l: list[str], size: int) -> list[Gate]:
+def x_data_gates(l: list[str], size: int) -> list[Gate]:
     """Generates quantum gates from a list of binary strings,
     the ih-gate action is to set initialized qubits to the i-th string in the given list.
 
@@ -80,11 +80,54 @@ def xor_data_gates(l: list[str], size: int) -> list[Gate]:
 
 
 def compute_lookup_table(W: int, l: list[int], optimization: int = 0) -> QuantumCircuit:
-    """
-    a = input; input size in bits is log2(len(l)), ex: if len(l) = 8, then input size is 3, log2(8)=3, 2^3 = 8
-    w = output size in bits
-    l = table to be computed
-    c = controll qubit
+    """Computes the lookup-table(QROM)`[1]`, the circuit takes an input `a` and has an effect of XOR'ing 
+    the corresponding value in the list `l` into the `w-bits` output register.
+
+    Exemple:
+    -
+    W = 6
+    l = any integer list of size 8
+
+    a0 ---○----○----○----○----●----●----●----●----
+          |    |    |    |    |    |    |    |
+    a1 ---○----○----●----●----○----○----●----●----
+          |    |    |    |    |    |    |    |
+    a2 ---○----●----○----●----○----●----○----●----
+          |    |    |    |    |    |    |    |
+    c  ---●----●----●----●----●----●----●----●----
+          |    |    |    |    |    |    |    |
+    W0 --| |--| |--| |--| |--| |--| |--| |--| |---
+         | |  | |  | |  | |  | |  | |  | |  | |
+    W1 --| |--| |--| |--| |--| |--| |--| |--| |---
+         | |  | |  | |  | |  | |  | |  | |  | |
+    W2 --|L|--|L|--|L|--|L|--|L|--|L|--|L|--|L|---
+         |0|  |1|  |2|  |3|  |4|  |5|  |6|  |7|
+    W3 --| |--| |--| |--| |--| |--| |--| |--| |---
+         | |  | |  | |  | |  | |  | |  | |  | |
+    W4 --| |--| |--| |--| |--| |--| |--| |--| |---
+         | |  | |  | |  | |  | |  | |  | |  | |
+    W5 --|_|--|_|--|_|--|_|--|_|--|_|--|_|--|_|---
+
+    Complexity:
+    -
+    Number of bits per register:
+
+    - `w`, output register, takes `W` bits.
+    - `a`, input register, takes `⌈log2|l|⌉` bits.
+    - `c`, control register, takes `1` bit.
+
+    Args:
+        W (int): output size in bits.
+        l (list[int]): list to be computed.
+        optimization (int): the level of optimization the circuit should have, `default = 0`.
+    
+    Returns:
+        quantum_circuit(QuantumCircuit): the quantum circuit implementing the lookup table (QROM).
+
+    Reference:
+    -
+    [1]Encoding Electronic Spectra in Quantum Circuits with Linear T Complexity.
+    Ryan Babbush, Craig Gidney, Dominic W. Berry, Nathan Wiebe, Jarrod McClean, Alexandru Paler, Austin Fowler, and Hartmut Neven
     """
     index_size = ceil(log2(len(l)))
     a = QuantumRegister(index_size, name="a")
@@ -94,7 +137,7 @@ def compute_lookup_table(W: int, l: list[int], optimization: int = 0) -> Quantum
 
     c_strings = generate_control_strings(len(l))
     e_table = encode_table(l, W)
-    xor_circs = xor_data_gates(e_table, W)
+    xor_circs = x_data_gates(e_table, W)
     
     match optimization:
         case 0:
