ede1103ae27598126d730c1d1b801cb3159ab5c4
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-06-28 11:33:37 -0300

    implementing in main .py file

--- implementations/grover_algorithm/grover.py
+++ implementations/grover_algorithm/grover.py
@@ -1,6 +1,8 @@
 from qiskit import *
-from qiskit.circuit.library import MCXGate, ZGate
-from math import pi
+from qiskit.circuit.library import MCXGate, ZGate, QFT
+from math import pi, sin
+from qiskit_aer import AerSimulator
+from qiskit import transpile
 
 def get_sat() -> tuple[int, list[list[int]]]:
     """
@@ -82,22 +84,82 @@ def inversion_by_the_mean(num_bits: int) -> QuantumCircuit:
     return quantum_circuit
 
 
-def groover(bit_array: list[int], sat_clauses: list[list[int]]) -> QuantumCircuit:
-    """
+def grover(bit_array: list[int], sat_clauses: list[list[int]]) -> QuantumCircuit:
+    """Grover Operator
     bit array is the array that contains the how many bits will be used in witch register
     [5,3,1] means 5 input bits, 3 anc bits and 1 result bit.
     """
     x = QuantumRegister(bit_array[0], "x")
     anc = QuantumRegister(bit_array[1], "anc")
-    r = QuantumRegister(bit_array[2], "r")  
-    result = ClassicalRegister(bit_array[0], "result")
-    quantum_circuit = QuantumCircuit(x,anc,r,result,name="grover-SAT")
-    quantum_circuit.h(x)
-    UG = sat_UG(bit_array, sat_UF(bit_array[0],sat_clauses))
-    inv = inversion_by_the_mean(bit_array[0])
-    num_reps = int((pi/4)*((2**bit_array[0])**(1/2)))
-    for _ in range(num_reps):
-        quantum_circuit.append(UG, x[:] + anc[:] + r[:])
-        quantum_circuit.append(inv, x[:])
-    quantum_circuit.measure(x, result)
+    r = QuantumRegister(bit_array[2], "r")
+    quantum_circuit = QuantumCircuit(x,anc,r,name="grover-SAT")
+    UG = sat_UG(bit_array, sat_UF(bit_array[0],sat_clauses).to_gate()).to_gate()
+    inv = inversion_by_the_mean(bit_array[0]).to_gate()
+    quantum_circuit.append(UG, x[:] + anc[:] + r[:])
+    quantum_circuit.append(inv, x[:])
     return quantum_circuit
+
+
+def grover_search(bit_arr: list[int], fnc: list[list[int]]) -> QuantumCircuit:
+    """
+    bit array [num_literals, num_anc, num_result]
+    """
+    G = grover(bit_arr, fnc)
+    x = QuantumRegister(bit_arr[0], "x")
+    anc = QuantumRegister(bit_arr[1], "anc")
+    r = QuantumRegister(bit_arr[2], "r")
+    result = ClassicalRegister(bit_arr[0], "result")
+    quantum_circuit = QuantumCircuit(x,anc,r,result)
+    quantum_circuit.h(x)
+    num_reps = int((pi/4)*((2**bit_arr[0])**(1/2)))
+    for i in range(num_reps):
+        quantum_circuit.append(G, x[:] + anc[:] + r[:])
+    quantum_circuit.measure(x,result)
+    return quantum_circuit
+
+
+def grover_counting(bit_arr: list[int], fnc: list[list[int]]) -> QuantumCircuit:
+    G = grover(bit_arr, fnc)
+    x = QuantumRegister(bit_arr[0], "x")
+    anc = QuantumRegister(bit_arr[1], "anc")
+    r = QuantumRegister(bit_arr[2], "r")
+    p = QuantumRegister(bit_arr[3], "p")
+    result = ClassicalRegister(bit_arr[3], "result")
+    quantum_circuit = QuantumCircuit(p,x,anc,r,result)
+    quantum_circuit.h(p)
+    quantum_circuit.h(x)
+    num_reps = bit_arr[3]
+    for i in range(num_reps):
+        quantum_circuit.append(G.power(2**i).control(1), p[i:i+1] + x[:] + anc[:] + r[:])
+    quantum_circuit.append(QFT(bit_arr[3], inverse=True), p[:])
+    quantum_circuit.measure(p, result)
+    return quantum_circuit
+
+
+def get_search_solution(GS: QuantumCircuit) -> str:
+    simulator_aer = AerSimulator()
+    qc_aer = transpile(GS, backend=simulator_aer)
+    job = simulator_aer.run(qc_aer, shots=(1<<15))
+    job_result = job.result()
+    counts = job_result.get_counts(qc_aer)
+    value = counts.most_frequent()
+    return value
+
+
+def get_counting_solution(bits: list[int], GC: QuantumCircuit) -> float:
+    """bits [n_literals, n_precision_bits]
+    """
+    simulator_aer = AerSimulator()
+    qc_aer = transpile(GC, backend=simulator_aer)
+    job = simulator_aer.run(qc_aer, shots=(1<<15))
+    job_result = job.result()
+    counts = job_result.get_counts(qc_aer)
+    measured_value = int(counts.most_frequent(),2)
+
+    n = 2**bits[0]
+    theta = 2*pi*measured_value/(2**bits[1])
+    m = n*(sin(theta/2))**2
+    print(f"M = {m}")
+    print(f"N = {n}")
+    print(f"Number of solutions: ~{n-m}")
+    return n-m
